0c4b83f914a26067ce18a1a7ae5ee029155a0f19
Tristan <tfount96@gmail.com>
2019-10-01 23:03:58 -0500

    Small commit

--- Components/Player/PlayerAbilities.cpp
+++ Components/Player/PlayerAbilities.cpp
@@ -2,9 +2,10 @@
 #include "Player.h"
 
 void PlayerAbilities::updateAbilityInputs() {
-
+    // Check if any inputs are held down, and queue the ability to be used if so, setting it on cooldown appropriately
+    // In other words, determine if we can use this ability and take care of "cleaning up" after it
 }
 
 void PlayerAbilities::applyActiveAbilities(World& world) {
-
+    // Use pointer to the relevant physics component and the world to do what's necessary for this ability
 }
